4a4f13280a3848ef26bf78b3a0e1e45b5cd186fb
Mnpr <acharya.sudesh366@gmail.com>
2024-01-28 13:32:30 +0100

    setup baseline seasonal arima

---
+++ 2_BaselineModel/utils.py
@@ -0,0 +1,39 @@
+import math
+import itertools
+from typing import Union
+
+import pandas as pd
+import statsmodels.api as sm
+
+
+def search_optimal_params(param_combinations: list, train_set: pd.DataFrame) -> tuple:
+    """
+    Search for the optimal parameters to minimize AIC in SARIMA model.
+    Parameters:
+        param_combinations: List of parameter combinations to try
+        train_set: Training set
+    Returns:
+        Tuple of best AIC and the corresponding best parameters
+    """
+    best_aic = float("inf")
+    best_params = None
+
+    for params in param_combinations:
+        order = params[:3]
+        seasonal_order = params[3:]
+
+        try:
+            model = sm.tsa.SARIMAX(train_set, order=order, seasonal_order=seasonal_order)
+            result = model.fit(disp=False)
+            aic = result.aic
+            if not math.isinf(result.zvalues.mean()):
+                print("order: {}, seasonal_order: {}, AIC: {}".format(order, seasonal_order, aic))
+                if aic < best_aic:
+                    best_aic = aic
+                    best_params = params
+            else:
+                print(order, seasonal_order, 'not converged')
+        except:
+            continue
+
+    return (best_aic, best_params)
